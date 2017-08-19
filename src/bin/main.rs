24b512bdf975b6c72e8d0329b9aab06f3396bbbd
cssivision <cssivision@gmail.com>
2017-08-19 23:00:03 +0800

    dependency

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,23 @@
+extern crate futures;
+use futures::future::*;
+
 fn main() {
-    println!("http2socks")
+    let f = join_all(vec![
+        ok::<u32, u32>(1),
+        ok::<u32, u32>(2),
+        ok::<u32, u32>(3),
+    ]);
+    let f = f.map(|x| {
+        assert_eq!(x, [1, 2, 3]);
+    });
+
+    let f = join_all(vec![
+        ok::<u32, u32>(1).boxed(),
+        err::<u32, u32>(2).boxed(),
+        ok::<u32, u32>(3).boxed(),
+    ]);
+    let f = f.then(|x| {
+        assert_eq!(x, Err(2));
+        x
+    });
 }
