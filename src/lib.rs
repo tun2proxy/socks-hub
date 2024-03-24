63630f739cea93d8a478504db472a0ceef34eb9e
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-03-24 15:23:34 +0800

    cbindgen workground with cfg_if
    this issue is here: https://github.com/mozilla/cbindgen/issues/935

--- src/lib.rs
+++ src/lib.rs
@@ -12,33 +12,52 @@ cfg_if::cfg_if! {
     }
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "sockshub")] {
+//
+// can't use cfg_if here, because it will cause cbindgen to can't generate ffi.h correctly.
+// see this issue: https://github.com/mozilla/cbindgen/issues/935
+//
+// cfg_if::cfg_if! {
+//     if #[cfg(feature = "sockshub")] {
 
+#[cfg(feature = "sockshub")]
 mod config;
+#[cfg(feature = "sockshub")]
 pub use config::{ArgVerbosity, Config, Credentials, ProxyType};
 
+#[cfg(feature = "sockshub")]
 mod tokiort;
+#[cfg(feature = "sockshub")]
 use tokiort::TokioIo;
 
+#[cfg(feature = "sockshub")]
 mod http2socks;
+#[cfg(feature = "sockshub")]
 mod socks2socks;
 
+#[cfg(feature = "sockshub")]
 mod api;
+#[cfg(feature = "sockshub")]
 mod dump_logger;
+#[cfg(feature = "sockshub")]
 mod ffi;
 
+#[cfg(feature = "sockshub")]
 pub type BoxError = Box<dyn std::error::Error + Send + Sync>;
+#[cfg(feature = "sockshub")]
 pub type Result<T, E = BoxError> = std::result::Result<T, E>;
 
+#[cfg(feature = "sockshub")]
 use socks5_impl::protocol::{Address, UserKey};
+#[cfg(feature = "sockshub")]
 use std::{net::SocketAddr, time::Duration};
+#[cfg(feature = "sockshub")]
 use tokio::{
     net::{TcpStream, ToSocketAddrs},
     sync::mpsc::Receiver,
     time::timeout,
 };
 
+#[cfg(feature = "sockshub")]
 pub async fn main_entry<F>(config: &Config, quit: Receiver<()>, callback: Option<F>) -> Result<(), BoxError>
 where
     F: FnOnce(SocketAddr) + Send + Sync + 'static,
@@ -49,8 +68,10 @@ where
     }
 }
 
+#[cfg(feature = "sockshub")]
 pub(crate) const CONNECT_TIMEOUT: Duration = Duration::from_secs(5);
 
+#[cfg(feature = "sockshub")]
 pub(crate) async fn create_s5_connect<A: ToSocketAddrs>(
     server: A,
     dur: Duration,
@@ -63,9 +84,10 @@ pub(crate) async fn create_s5_connect<A: ToSocketAddrs>(
     Ok(stream)
 }
 
+#[cfg(feature = "sockshub")]
 pub(crate) fn std_io_error_other<E: Into<BoxError>>(err: E) -> std::io::Error {
     std::io::Error::new(std::io::ErrorKind::Other, err)
 }
 
-    }
-}
+//     }
+// }
